b5985c1b4a6976ebe317b8b869a68f7722b89d2f
Denys1994q <drybachok@gmail.com>
2023-10-07 21:09:39 +0300

    fix cookies17

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(
         resave: false,
         saveUninitialized: true,
         cookie: {
-            sameSite: "strict",
+            sameSite: "None",
             secure: true,
         },
     })
